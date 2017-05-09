60414d054d5295f9ebe5b9d0cba9127881a1b931
James Duncan <james.a.duncan91@gmail.com>
2017-05-09 17:02:07 +0100

    added function to remove dinos and count ones which have more than two babies annually

--- park.js
+++ park.js
@@ -5,6 +5,23 @@ var Park = function(){
 Park.prototype = {
   add: function(dinosaur){
     this.dinosaurs.push(dinosaur);
+  },
+  removeDino: function (name) {
+    for (var dinosaur of this.dinosaurs) {
+      if (dinosaur.type === name) {
+        var index = this.dinosaurs.indexOf(dinosaur);
+        this.dinosaurs.splice(index, 1);
+      }
+    }
+  },
+  checkRapidBabyMakers: function(){
+    var counter = 0;
+    for (var dinosaur of this.dinosaurs){
+      if(dinosaur.annualOffspring >2 ){
+        counter ++;
+      }
+    }
+    return counter;
   }
 }
   module.exports = Park;
--- specs/park_spec.js
+++ specs/park_spec.js
@@ -7,15 +7,31 @@ describe("Park", function(){
   beforeEach(function(){
     this.park = new Park();
     this.dinosaur = new Dinosaur("T-Rex", 1);
+    this.dinosaur2 = new Dinosaur("Raptor", 3);
+    this.dinosaur3 = new Dinosaur("Compy", 5);
   })
 
   it("dinosaurs should start empty", function(){
     assert.equal(0, this.park.dinosaurs.length);
   })
 
-  it("can add item to dinosaurs", function(){
+  it("can add dinosaur to dinosaurs", function(){
     this.park.add(this.dinosaur);
     assert.equal(1, this.park.dinosaurs.length);
+  })
+
+  it("can remove dinosaur type from dinosaurs", function(){
+    this.park.add(this.dinosaur);
+    this.park.removeDino("T-Rex");
+    assert.equal(0, this.park.dinosaurs.length);
+  })
+
+  it("can get all dinosaurs with annualOffspring of more than 2", function(){
+    this.park.add(this.dinosaur);
+    this.park.add(this.dinosaur2);
+    this.park.add(this.dinosaur3);
+    var result = this.park.checkRapidBabyMakers();
+    assert.equal(2, result);
   })
 
 })
